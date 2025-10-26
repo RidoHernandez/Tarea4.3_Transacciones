3da233cdb158992329ea23461f2c00fc4701910e
Ricardo Pérez Hernández <ricardo123perezhdz56@gmail.com>
2025-10-26 16:10:07 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Tarea4._3_Transaccion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            txtCode = new TextBox();
+            btnDescontinuar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Diseño base ";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(146, 127);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(515, 214);
+            dataGridView1.TabIndex = 1;
+            // 
+            // txtCode
+            // 
+            txtCode.Location = new Point(436, 41);
+            txtCode.Name = "txtCode";
+            txtCode.Size = new Size(185, 23);
+            txtCode.TabIndex = 2;
+            // 
+            // btnDescontinuar
+            // 
+            btnDescontinuar.Location = new Point(377, 390);
+            btnDescontinuar.Name = "btnDescontinuar";
+            btnDescontinuar.Size = new Size(125, 23);
+            btnDescontinuar.TabIndex = 4;
+            btnDescontinuar.Text = "Descontinuar";
+            btnDescontinuar.UseVisualStyleBackColor = true;
+            btnDescontinuar.Click += btnDescontinuar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDescontinuar);
+            Controls.Add(txtCode);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private DataGridView dataGridView1;
+        private TextBox txtCode;
+        private Button btnDescontinuar;
+    }
+}
